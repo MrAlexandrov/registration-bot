1ce878d23b4336b2dcc3258a956a0e04d628de56
Maxim Alexandrov <MrAlexandrov0@yandex.ru>
2025-03-06 22:01:38 +0000

    Save changes

--- dump.sh
+++ dump.sh
@@ -1,20 +1,45 @@
 #!/bin/bash
 
+ROOT_DIRECTORY="$HOME/registration-bot"
+
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+echo "Dump started at ${TIMESTAMP}" | tee -a "${ROOT_DIRECTORY}/debug.log"
+
 # Путь к вашей базе данных
-DATABASE_PATH="database.sqlite"
+DATABASE_PATH="${ROOT_DIRECTORY}/database.sqlite"
+SOURCE_LOG="${ROOT_DIRECTORY}/screenlog.0"
 
 # Директория для хранения дампов
-DUMP_DIR="dumps"
+DUMP_DIR="${ROOT_DIRECTORY}/dumps"
 
 # Убедиться, что директория для дампов существует
-mkdir -p $DUMP_DIR
+mkdir -p "$DUMP_DIR"
+if [ $? -ne 0 ]; then
+    echo "Ошибка создания директории $DUMP_DIR" | tee -a "${ROOT_DIRECTORY}/debug.log"
+    exit 1
+fi
 
 # Генерация уникального имени с использованием метки времени
-TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 DUMP_NAME="dump_$TIMESTAMP.sqlite"
 DUMP_PATH="$DUMP_DIR/$DUMP_NAME"
 
 # Копирование базы данных
-cp "$DATABASE_PATH" "$DUMP_PATH"
+if ! cp "$DATABASE_PATH" "$DUMP_PATH"; then
+    echo "Ошибка при копировании базы данных" | tee -a "${ROOT_DIRECTORY}/debug.log"
+else
+    echo "База данных сохранена в $DUMP_PATH" | tee -a "${ROOT_DIRECTORY}/debug.log"
+    sudo chattr +i "$DUMP_PATH"
+fi
 
-echo "База данных сохранена как $DUMP_PATH"
+# Копирование логов
+if [ -f "$SOURCE_LOG" ]; then
+    if cp "$SOURCE_LOG" "$DUMP_DIR/screenlog_$TIMESTAMP.log"; then
+        rm "$SOURCE_LOG"
+        echo "Лог сохранён в $DUMP_DIR/screenlog_$TIMESTAMP.log" | tee -a "${ROOT_DIRECTORY}/debug.log"
+        sudo chattr +i "$DUMP_DIR/screenlog_$TIMESTAMP.log"
+    else
+        echo "Ошибка при копировании лога $SOURCE_LOG" | tee -a "${ROOT_DIRECTORY}/debug.log"
+    fi
+else
+    echo "Файл $SOURCE_LOG не найден" | tee -a "${ROOT_DIRECTORY}/debug.log"
+fi
